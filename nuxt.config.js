e8a585f0f87b2c4791c6de698f084ae4bd63795c
juac08 <juni430@gmail.com>
2021-09-23 20:06:13 +0200

    PWA Apple devices

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,20 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" }
+      { name: "format-detection", content: "telephone=no" },
+      { hid: "description", name: "description", content: "" },
+      {
+        name: "msapplication-TileColor",
+        content: "#404987"
+      },
+      {
+        name: "msapplication-TileImage",
+        content: "/favicon/ms-icon-144x144.png"
+      },
+      {
+        name: "theme-color",
+        content: "#404987"
+      }
     ],
 
     link: [
