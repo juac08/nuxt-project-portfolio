a81799cd7232f331a94a7fcaa921f70aa4068ce0
juac08 <juni430@gmail.com>
2021-10-16 21:17:40 +0200

    push notification

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,6 @@
 import { Vue, Component } from "vue-property-decorator";
 import Hero from "~/components/Hero/index.vue";
 import axios from "axios";
-import { AxiosOptions } from "@nuxtjs/axios";
 @Component({
   head: {
     title: "Portfolio | Home"
@@ -20,36 +19,37 @@ import { AxiosOptions } from "@nuxtjs/axios";
 export default class index extends Vue {
   /* Props */
   /* State */
-  // async mounted() {
-  //   const currentToken = await this.$fire.messaging.getToken();
-  //   const data = JSON.stringify({
-  //     notification: {
-  //       title: "firebase",
-  //       body: "firebase is awesome",
-  //       click_action: "http://localhost:3000/",
-  //       icon: "http://localhost:3000/static/icon.png"
-  //     },
-  //     to: currentToken
-  //   });
-  //   const config = {
-  //     method: "post",
-  //     url: "https://fcm.googleapis.com/fcm/send",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization:
-  //         "key=AAAA7MmYUsc:APA91bFoA0q5oxaWxmXbcw6Nz8dbPi24UD2ZzLNNqoQy4I6lEMLKB54Ttk5WQFn_YJk7gSw-X2yn6lJYz8g-SDsg3Gp14f2Rj-b0u_qUglFM3oih-6zBrBfIspsNqsSTjrrGed4vGCOh"
-  //     },
-  //     data
-  //   };
-  //   const response = await axios(config);
-  //   this.$fire.messaging.onMessage(payload => {
-  //     console.info("Message received: ", payload);
-  //   });
-  //   this.$fire.messaging.onTokenRefresh(async () => {
-  //     const refreshToken = await this.$fire.messaging.getToken();
-  //     console.log("Token Refreshed", refreshToken);
-  //   });
-  // }
+  async mounted() {
+    const currentToken = await this.$fire.messaging.getToken();
+    const data = JSON.stringify({
+      notification: {
+        title: "firebase",
+        body: "firebase is awesome",
+        click_action: "http://localhost:3000/",
+        icon: "http://localhost:3000/static/icon.png"
+      },
+      to: currentToken
+    });
+    const config = {
+      method: "post",
+      url: "https://fcm.googleapis.com/fcm/send",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "key=AAAA7MmYUsc:APA91bFoA0q5oxaWxmXbcw6Nz8dbPi24UD2ZzLNNqoQy4I6lEMLKB54Ttk5WQFn_YJk7gSw-X2yn6lJYz8g-SDsg3Gp14f2Rj-b0u_qUglFM3oih-6zBrBfIspsNqsSTjrrGed4vGCOh"
+      },
+      data
+    };
+    // @ts-expect-error
+    const response = await this.$axios(config);
+    this.$fire.messaging.onMessage(payload => {
+      console.info("Message received: ", payload);
+    });
+    this.$fire.messaging.onTokenRefresh(async () => {
+      const refreshToken = await this.$fire.messaging.getToken();
+      console.log("Token Refreshed", refreshToken);
+    });
+  }
   /* Data */
   /* Hooks */
   /* Methods */
